2fe7e81bbf554e4e53bb4d513b1c3f10dab8ad80
lgaetz <lgaetz@gmail.com>
2013-04-27 16:50:55 -0300

    clean up auto check for update

--- functions.inc.php
+++ functions.inc.php
@@ -140,17 +140,162 @@ function trunkbalance_vercheck() {
 	// compare version numbers of local module.xml and remote module.xml 
 	// returns true if a new version is available
 	$newver = false;
-	if ( function_exists(xml2array)){
-		$module_local = xml2array("modules/trunkbalance/module.xml");
-		$module_remote = xml2array("https://raw.github.com/POSSA/freepbx-trunk-balancing/master/module.xml");
-		if ( $foo= empty($module_local) or $bar = empty($module_remote) )
+	$module_local = trunkbalance_xml2array("modules/trunkbalance/module.xml");
+	$module_remote = trunkbalance_xml2array("https://raw.github.com/POSSA/freepbx-trunk-balancing/master/module.xml");
+	if ( $foo= empty($module_local) or $bar = empty($module_remote) )
+	{
+		//  if either array is empty skip version check
+	}
+	else if ( $module_remote[module][version] > $module_local[module][version])
+	{
+		$newver = true;
+	}
+	return ($newver);
+}
+
+	//Parse XML file into an array
+function trunkbalance_xml2array($url, $get_attributes = 1, $priority = 'tag')  {
+	$contents = "";
+	if (!function_exists('xml_parser_create'))
+	{
+		return array ();
+	}
+	$parser = xml_parser_create('');
+	if(!($fp = @ fopen($url, 'rb')))
+	{
+		return array ();
+	}
+	while(!feof($fp))
+	{
+		$contents .= fread($fp, 8192);
+	}
+	fclose($fp);
+	xml_parser_set_option($parser, XML_OPTION_TARGET_ENCODING, "UTF-8");
+	xml_parser_set_option($parser, XML_OPTION_CASE_FOLDING, 0);
+	xml_parser_set_option($parser, XML_OPTION_SKIP_WHITE, 1);
+	xml_parse_into_struct($parser, trim($contents), $xml_values);
+	xml_parser_free($parser);
+	if(!$xml_values)
+	{
+		return; //Hmm...
+	}
+	$xml_array = array ();
+	$parents = array ();
+	$opened_tags = array ();
+	$arr = array ();
+	$current = & $xml_array;
+	$repeated_tag_index = array ();
+	foreach ($xml_values as $data)
+	{
+		unset ($attributes, $value);
+		extract($data);
+		$result = array ();
+		$attributes_data = array ();
+		if (isset ($value))
+		{
+			if($priority == 'tag')
 			{
-			//  if either array is empty skip version check
+				$result = $value;
 			}
-		else if ( $module_remote[module][version] > $module_local[module][version])
+			else
 			{
-			$newver = true;
+				$result['value'] = $value;
 			}
-		return ($newver);
+		}
+		if(isset($attributes) and $get_attributes)
+		{
+			foreach($attributes as $attr => $val)
+			{
+				if($priority == 'tag')
+				{
+					$attributes_data[$attr] = $val;
+				}
+				else
+				{
+					$result['attr'][$attr] = $val; //Set all the attributes in a array called 'attr'
+				}
+			}
+		}
+		if ($type == "open")
+		{
+			$parent[$level -1] = & $current;
+			if(!is_array($current) or (!in_array($tag, array_keys($current))))
+			{
+				$current[$tag] = $result;
+				if($attributes_data)
+				{
+					$current[$tag . '_attr'] = $attributes_data;
+				}
+				$repeated_tag_index[$tag . '_' . $level] = 1;
+				$current = & $current[$tag];
+			}
+			else
+			{
+				if (isset ($current[$tag][0]))
+				{
+					$current[$tag][$repeated_tag_index[$tag . '_' . $level]] = $result;
+					$repeated_tag_index[$tag . '_' . $level]++;
+				}
+				else
+				{
+					$current[$tag] = array($current[$tag],$result);
+					$repeated_tag_index[$tag . '_' . $level] = 2;
+					if(isset($current[$tag . '_attr']))
+					{
+						$current[$tag]['0_attr'] = $current[$tag . '_attr'];
+						unset ($current[$tag . '_attr']);
+					}
+				}
+				$last_item_index = $repeated_tag_index[$tag . '_' . $level] - 1;
+				$current = & $current[$tag][$last_item_index];
+			}
+		}
+		else if($type == "complete")
+		{
+			if(!isset ($current[$tag]))
+			{
+				$current[$tag] = $result;
+				$repeated_tag_index[$tag . '_' . $level] = 1;
+				if($priority == 'tag' and $attributes_data)
+				{
+					$current[$tag . '_attr'] = $attributes_data;
+				}
+			}
+			else
+			{
+				if (isset ($current[$tag][0]) and is_array($current[$tag]))
+				{
+					$current[$tag][$repeated_tag_index[$tag . '_' . $level]] = $result;
+					if ($priority == 'tag' and $get_attributes and $attributes_data)
+					{
+						$current[$tag][$repeated_tag_index[$tag . '_' . $level] . '_attr'] = $attributes_data;
+					}
+					$repeated_tag_index[$tag . '_' . $level]++;
+				}
+				else
+				{
+					$current[$tag] = array($current[$tag],$result);
+					$repeated_tag_index[$tag . '_' . $level] = 1;
+					if ($priority == 'tag' and $get_attributes)
+					{
+						if (isset ($current[$tag . '_attr']))
+						{
+							$current[$tag]['0_attr'] = $current[$tag . '_attr'];
+							unset ($current[$tag . '_attr']);
+						}
+						if ($attributes_data)
+						{
+							$current[$tag][$repeated_tag_index[$tag . '_' . $level] . '_attr'] = $attributes_data;
+						}
+					}
+					$repeated_tag_index[$tag . '_' . $level]++; //0 and 1 index is already taken
+				}
+			}
+		}
+		else if($type == 'close')
+		{
+			$current = & $parent[$level -1];
 		}
 	}
+	return ($xml_array);
+}
