8bb38750099e0359ef14f4abc699aa8590c0200b
Lorne Gaetz <lgaetz@gmail.com>
2013-05-16 13:28:25 -0300

    Update page.trunkbalance.php
    major reorganization/clarification of GUI with introduction of new variables and features that are NOT YET SUPPORTED by functions.inc.php.

--- page.trunkbalance.php
+++ page.trunkbalance.php
@@ -91,8 +91,8 @@ if ($online_updates && $foo = trunkbalance_vercheck()) {
 <form autocomplete="off" name="edit" action="<?php $_SERVER['PHP_SELF'] ?>" method="post" onsubmit="return edit_onsubmit();">
 	<input type="hidden" name="display" value="<?php echo $dispnum?>">
 	<input type="hidden" name="action" value="<?php echo ($itemid ? 'edit' : 'add') ?>">
-	<table>
-	<tr><td colspan="2"><h5><?php echo ($itemid ? _("Edit Trunk") : _("Add Trunk")) ?><hr></h5></td></tr>
+<table>
+	<tr><td colspan="2"><h3><?php echo ($itemid ? _("Edit Trunk") : _("Add Trunk")) ?><hr></h3></td></tr>
 
 <?php		if ($itemid){ ?>
 		<input type="hidden" name="itemid" value="<?php echo $itemid; ?>">
@@ -102,94 +102,118 @@ if ($online_updates && $foo = trunkbalance_vercheck()) {
 		<td><a href="#" class="info"><?php echo _("Trunk Description:")?><span><?php echo _("Enter a description for this balanced trunk.")?></span></a></td>
 		<td><input type="text" name="description" value="<?php echo (isset($thisItem['description']) ? $thisItem['description'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
-       <tr>
-	<td><a href="#" class="info"><?php echo _("Trunk Destination:")?><span><?php echo _("Select the destination trunk")?></span></a></td>
-	<td><SELECT id="desttrunk_id" name="desttrunk_id" tabindex="<?php echo ++$tabindex;?>"><OPTION VALUE="0">Select...</option>
+	<tr>
+		<td><a href="#" class="info"><?php echo _("Trunk Destination:")?><span><?php echo _("Select the destination trunk")?></span></a></td>
+		<td><SELECT id="desttrunk_id" name="desttrunk_id" tabindex="<?php echo ++$tabindex;?>"><OPTION VALUE="0">Select...</option>
 <?php     
-	    $trunklist = trunkbalance_listtrunk();
-           foreach ($trunklist as $trunk){
-		if ($trunk['trunkid']!=0){
-		   echo _("<OPTION VALUE=\"");
-	    	   echo ($trunk['trunkid']);
-	          echo _("\"");
-		   if ($thisItem['desttrunk_id']==$trunk['trunkid']) echo _("selected=\"selected\"");
-		   echo _(">");
-	          echo($trunk['name'].' ('.$trunk['tech'].')');
-	          echo _("</OPTION>");
-		} 
-	    }?>
-</SELECT></td>
-       </tr>
-       <tr>
-	<td><a href="#" class="info"><?php echo _("Time Group:")?><span><?php echo _("Select the time group condition")?></span></a></td>
-	<td><SELECT id="timegroup_id" name="timegroup_id" tabindex="<?php echo ++$tabindex;?>"><OPTION VALUE="-1">none selected</option>
-           <?php     
-	    $timegrouplist = trunkbalance_listtimegroup();
-           foreach ($timegrouplist as $timegroup){
-		if ($timegroup['id']!=0){
-		   echo _("<OPTION VALUE=\"");
-	    	   echo ($timegroup['id']);
-	          echo _("\"");
-		   if ($thisItem['timegroup_id']==$timegroup['id']) echo _("selected=\"selected\"");
-		   echo _(">");
-	          echo($timegroup['description']);
-	          echo _("</OPTION>");
-		} 
-	    }?>
-</SELECT></td>
-       </tr>
-
-       <tr>
+			$trunklist = trunkbalance_listtrunk();
+			   foreach ($trunklist as $trunk){
+			if ($trunk['trunkid']!=0){
+			   echo _("<OPTION VALUE=\"");
+				   echo ($trunk['trunkid']);
+				  echo _("\"");
+			   if ($thisItem['desttrunk_id']==$trunk['trunkid']) echo _("selected=\"selected\"");
+			   echo _(">");
+				  echo($trunk['name'].' ('.$trunk['tech'].')');
+				  echo _("</OPTION>");
+			} 
+			}?>
+		</SELECT></td>
+	</tr>
+		<tr><td><a href="#" class="info"><?php echo _("Time Group:")?><span><?php echo _("Trunk is only active during the times specified in the selected time group.")?></span></a></td>
+		<td><SELECT id="timegroup_id" name="timegroup_id" tabindex="<?php echo ++$tabindex;?>"><OPTION VALUE="-1">none selected</option>
+			   <?php     
+			$timegrouplist = trunkbalance_listtimegroup();
+			   foreach ($timegrouplist as $timegroup){
+			if ($timegroup['id']!=0){
+			   echo _("<OPTION VALUE=\"");
+				   echo ($timegroup['id']);
+				  echo _("\"");
+			   if ($thisItem['timegroup_id']==$timegroup['id']) echo _("selected=\"selected\"");
+			   echo _(">");
+				  echo($timegroup['description']);
+				  echo _("</OPTION>");
+			} 
+			}?>
+		</SELECT></td>
+	</tr>
+	<tr>
 		<td><a href="#" class="info"><?php echo _("Matching Rule:")?><span><?php echo _("Enter the SQL matching pattern that will be applied to the CDR to calculate your rules on this trunk. It will be inserted as WHERE dst LIKE 'your pattern'. For instance if you want to match all numbers starting by 0033 you will enter 0033%. At this time only one pattern will work. ")?></span></a></td>
 		<td><input type="textarea" rows="5" cols="25" name="dialpattern" value="<?php echo (isset($thisItem['dialpattern']) ? $thisItem['dialpattern'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
-
 	<tr>
 		<td><a href="#" class="info"><?php echo _("Not Matching Rule:")?><span><?php echo _("Enter the matching pattern that will be excluded from the CDR matching to calculate your rules on this trunk.It will be inserted as WHERE dst NOT LIKE 'your pattern'. ")?></span></a></td>
 		<td><input type="textarea" rows="5" cols="25" name="notdialpattern" value="<?php echo (isset($thisItem['notdialpattern']) ? $thisItem['notdialpattern'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
 
-
-       <tr>
-		<td><a href="#" class="info"><?php echo _("Billing Day:")?><span><?php echo _("Enter the day of the month when to reset the counter. 0 for never reseting the counter. If this field is used, you should set Billing Period to 0")?></span></a></td>
-		<td><input type="text" name="billingday" value="<?php echo (isset($thisItem['billingday']) ? $thisItem['billingday'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+	<tr><td colspan="2"><h5>Billing Cycle Configuration<hr></h5></td></tr>
+		<td><a href="#" class="info"><?php echo _("Choose billing cycle:")?><span><?php echo _("Choose the time period that the billing cycle will resest to")?></span></a></td>
+		<td><SELECT id="billing_cycle" name="billing_cycle" tabindex="<?php echo ++$tabindex;?>"><OPTION VALUE="-1">none selected</option>
+		<OPTION VALUE="day">Floating</OPTION>
+		<OPTION VALUE="day">Day</OPTION>
+		<OPTION VALUE="week">Week</OPTION>
+		<OPTION VALUE="month">Month</OPTION>
+		</SELECT></td>
+    </tr>
+	<tr>
+		<td><a href="#" class="info"><?php echo _("Billing Time:")?><span><?php echo _("Enter the time of day to reset the counter. Used for all non floating billing cycles.")?></span></a></td>
+		<td><input type="text" name="billingtime" value="<?php echo (isset($thisItem['billingtime']) ? $thisItem['billingtime'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
 	<tr>
-		<td><a href="#" class="info"><?php echo _("Billing Period :")?><span><?php echo _("Enter the number of floating days that should be included in the count. 0 to include all. If this field is used, you should set Billing Days to 0.")?></span></a></td>
-		<td><input type="text" name="billingperiod" value="<?php echo (isset($thisItem['billingperiod']) ? $thisItem['billingperiod'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+		<td><a href="#" class="info"><?php echo _("Billing Day:")?><span><?php echo _("Enter the day of the week to reset the counter. Only used for weekly billing cycle.")?></span></a></td>
+		
+		<td><SELECT id="billing_cycle" name="billing_cycle" tabindex="<?php echo ++$tabindex;?>"><OPTION VALUE="-1">none selected</option>
+			<OPTION VALUE="sunday">Sunday</OPTION>
+			<OPTION VALUE="monday">Monday</OPTION>
+			<OPTION VALUE="tuesday">Tuesday</OPTION>
+			<OPTION VALUE="wednesday">Wednesday</OPTION>
+			<OPTION VALUE="thursday">Thursday</OPTION>
+			<OPTION VALUE="friday">Friday</OPTION>
+			<OPTION VALUE="saturday">Saturday</OPTION>
+		</SELECT></td>
 	</tr>
-	
-
-       <tr>
+	<tr>
+		<td><a href="#" class="info"><?php echo _("Billing Date:")?><span><?php echo _("Enter the day of the month to reset the counter. Only used for Monthly billing cycle.")?></span></a></td>
+		<td><input type="number" name="billingdate" value="<?php echo (isset($thisItem['billingdate']) ? $thisItem['billingdate'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+	</tr>
+	<tr>
+		<td><a href="#" class="info"><?php echo _("Floating Billing Time:")?><span><?php echo _("Enter the number of floating hours that should be included in the count. 0 to include all. This is only used for the floating billing cycle.")?></span></a></td>
+		<td><input type="number" name="billingperiod" value="<?php echo (isset($thisItem['billingperiod']) ? $thisItem['billingperiod'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+	</tr>
+	<tr>
 		<td><a href="#" class="info"><?php echo _("Ending Date:")?><span><?php echo _("Enter the date when this balanced trunk should expire. YYYY-MM-DD HH:mm - Keep empty to disable")?></span></a></td>
 		<td><input type="text" name="endingdate" value="<?php echo (isset($thisItem['endingdate']) ? $thisItem['endingdate'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
-
-       <tr>
-		<td><a href="#" class="info"><?php echo _("Load Ratio:")?><span><?php echo _("Enter the ratio this trunk should accept call. For instance if you want to balance equaly between this balanced trunk and two following ones, you should enter 3 to let this trunk accept 1 out of 3 calls.")?></span></a></td>
-		<td><input type="text" name="loadratio" value="<?php echo (isset($thisItem['loadratio']) ? $thisItem['loadratio'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+	<tr><td colspan="2"><h5>Usage Limits Configuration<hr></h5></td></tr>
+	<tr>
+		<td><a href="#" class="info"><?php echo _("Include Inbound Calls:")?><span><?php echo _("Outbound calls are counted automatically, enable this setting to include inbound calls when determining usage limits.")?></span></a></td>
+		<td><input type="checkbox" name="count_inbound" value="<?php echo (isset($thisItem['count_inbound']) ? $thisItem['count_inbound'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
-
-       <tr>
-		<td><a href="#" class="info"><?php echo _("Maximum Time:")?><span><?php echo _("Enter the maximum number of minutes per billing period. Be aware that the test is performed before the begining of the call and it will not break an active call even if it lasts two hours")?></span></a></td>
-		<td><input type="text" name="maxtime" value="<?php echo (isset($thisItem['maxtime']) ? $thisItem['maxtime'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+	<tr>
+		<td><a href="#" class="info"><?php echo _("Include Unanswered Calls:")?><span><?php echo _("Answered calls are counted automatically, enable this setting to include unanswered calls when determining usage limits.")?></span></a></td>
+		<td><input type="checkbox" name="count_unanswered" value="<?php echo (isset($thisItem['count_unanswered']) ? $thisItem['count_unanswered'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+	</tr>	
+	<tr>
+		<td><a href="#" class="info"><?php echo _("Maximum inbound/outbound Calling Time:")?><span><?php echo _("Enter the maximum total number of calling minutes per billing period. Be aware that the test is performed before the begining of the call and it will not break an active call.")?></span></a></td>
+		<td><input type="number" name="maxtime" value="<?php echo (isset($thisItem['maxtime']) ? $thisItem['maxtime'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
-
-       <tr>
-		<td><a href="#" class="info"><?php echo _("Maximum Number of Calls:")?><span><?php echo _("Enter the maximum number of call per billing period.")?></span></a></td>
-		<td><input type="text" name="maxnumber" value="<?php echo (isset($thisItem['maxnumber']) ? $thisItem['maxnumber'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+	<tr>
+		<td><a href="#" class="info"><?php echo _("Maximum Number of inbound/outbound Calls:")?><span><?php echo _("Enter the maximum number of calls per billing period.")?></span></a></td>
+		<td><input type="number" name="maxnumber" value="<?php echo (isset($thisItem['maxnumber']) ? $thisItem['maxnumber'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
-
-       <tr>
-		<td><a href="#" class="info"><?php echo _("Max. Different Calls:")?><span><?php echo _("Enter the maximum of different phone numbers dialled per billing period")?></span></a></td>
-		<td><input type="text" name="maxidentical" value="<?php echo (isset($thisItem['maxidentical']) ? $thisItem['maxidentical'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+	<tr>
+		<td><a href="#" class="info"><?php echo _("Max. Number of Different outbound Calls:")?><span><?php echo _("Enter the maximum number of different outbound phone numbers allowed per billing period. The include inbound calls and include unanswered calls settings do not apply to this item.")?></span></a></td>
+		<td><input type="number" name="maxidentical" value="<?php echo (isset($thisItem['maxidentical']) ? $thisItem['maxidentical'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
 	</tr>
-
+	<tr><td colspan="2"><h5>Load Balancing Configuration<hr></h5></td></tr>
+	<tr>
+		<td><a href="#" class="info"><?php echo _("Load Ratio:")?><span><?php echo _("Enter the ratio of calls that this trunk should accept. For instance to allow 1/3 of outbound calls to complete, you should enter 3 to let this trunk accept 1 out of 3 calls.")?></span></a></td>
+		<td><input type="number" name="loadratio" value="<?php echo (isset($thisItem['loadratio']) ? $thisItem['loadratio'] : ''); ?>" tabindex="<?php echo ++$tabindex;?>"></td>
+	</tr>
 	<tr>
 		<td colspan="2"><br><h6><input name="submit" type="submit" value="<?php echo _("Submit Changes")?>" tabindex="<?php echo ++$tabindex;?>"></h6></td>		
 	</tr>
-
-	</table>
+</table>
 
 <p align="center" style="font-size:11px;"><br>
 The module is maintained by the developer community at the <a target="_blank" href="https://github.com/POSSA/freepbx-trunk-balancing"> PBX Open Source Software Alliance</a><br></p>
